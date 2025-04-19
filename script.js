a994e1e76e859fc696ad68cf58d89eb30937dda6
Manish Thakur <manisthakur2004@gmail.com>
2025-04-19 18:35:34 +0545

    updated files

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-
+    
 const box1 = document.getElementById('box1');
 const myH1 = document.getElementById('myh1');
 let move = 10;
 let x = 0;
-let y = 0;
+let y = 100;
 
 document.addEventListener("keydown", (event) => {
+    const boxWidth = box1.offsetWidth;
+    const boxHeight = box1.offsetHeight;
+    const maxX =  window.innerWidth - boxWidth;
+    const maxY =window.innerHeight -  boxHeight;
     if (event.key.startsWith("Arrow")) {
 
         switch (event.key) {
             case "ArrowUp":
+                if(y - move >= 100)
                 y -= move;
                 break;
             case "ArrowDown":
+                if(y + move <= maxY)
                 y += move;
                 break;
             case "ArrowRight":
+            if(y + move <= maxX)
                 x += move;
                 break;
             case "ArrowLeft":
+                if(x-move >= 0)
                 x -= move;
                 break;
         }
